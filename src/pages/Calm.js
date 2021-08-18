eb31ca33f68880362576d89b200c0cc8b5e00266
jordan powell <jordan.powell@siriusxm.com>
2021-08-18 09:24:17 -0400

    ref: create custom hook to handle calm probablity

--- src/pages/Calm.js
+++ src/pages/Calm.js
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { navigate } from "@reach/router";
-
 import { notion, useNotion } from "../services/notion";
 import { Nav } from "../components/Nav";
+import { map } from 'rxjs/operators';
 
-export function Calm() {
+function useCalm() {
   const { user } = useNotion();
-  const [calm, setCalm] = useState(0);
-
-  useEffect(() => {
-    if (!user) {
-      navigate("/login");
-    }
-  }, [user]);
+  const [calm, setCalm] = useState(user.calm);
 
   useEffect(() => {
     if (!user) {
       return;
     }
 
-    const subscription = notion.calm().subscribe((calm) => {
-      const calmScore = Math.trunc(calm.probability * 100);
-      setCalm(calmScore);
-    });
+    const subscription = notion.calm().pipe(
+      map(calm => calm.probability),
+      map(probability => Math.trunc(probability * 100)),
+    ).subscribe(probability => setCalm(probability));
 
-    return () => {
-      subscription.unsubscribe();
-    };
+    return () => subscription.unsubscribe();
+
   }, [user]);
+
+  return calm;
+}
+
+export function Calm() {
+  const { user } = useNotion();
+  const calm = useCalm();
 
   return (
     <main className="main-container">
